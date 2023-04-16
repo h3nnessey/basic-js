6a6a7211db48258f58fa57f695e705b490f5dede
Vitaliy <vitaliy.zayakin@gmail.com>
2023-04-16 16:30:27 +0500

    feat: add solution for mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,5 @@
 const { NotImplementedError } = require('../extensions/index.js');
+const { json } = require('mocha/lib/reporters');
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,9 +24,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const minesField = [];
+
+  matrix.forEach((row, i) => {
+    minesField[i] = [];
+    row.forEach((cell, j) => {
+      const prevRow = matrix[i - 1];
+      const nextRow = matrix[i + 1];
+      const north = prevRow ? prevRow[j] : null;
+      const northWest = prevRow ? prevRow[j - 1] : null;
+      const northEast = prevRow ? prevRow[j + 1] : null;
+      const west = row[j - 1] || null;
+      const east = row[j + 1] || null;
+      const south = nextRow ? nextRow[j] : null;
+      const southWest = nextRow ? nextRow[j - 1] : null;
+      const southEast = nextRow ? nextRow[j + 1] : null;
+
+      const allDirections = [north, northWest, northEast, west, east, south, southWest, southEast];
+      const minesAroundCount = allDirections.reduce((acc, curr) => (curr === true ? acc + 1 : acc), 0);
+      minesField[i].push(minesAroundCount);
+    });
+  });
+
+  return minesField;
 }
 
 module.exports = {
