8d804899202de07054f565fd7ab5464a26351c05
Vitaliy <vitaliy.zayakin@gmail.com>
2023-04-17 09:55:00 +0500

    feat: add solution for vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,63 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(directMachine = true) {
+    this.directMachine = directMachine;
+    this.alphabet = Array(26)
+      .fill('')
+      .map((_, i) => String.fromCharCode(i + 65))
+      .join('');
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  validateArgs(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+  }
+
+  getUpperCasedArgs(message, key) {
+    return [message.toUpperCase(), key.toUpperCase()];
+  }
+
+  encrypt(message, key) {
+    this.validateArgs(message, key);
+    [message, key] = this.getUpperCasedArgs(message, key);
+
+    let keyOffset = 0;
+
+    const encryptedMessage = [...message].reduce((acc, curr, i) => {
+      if (this.alphabet.includes(curr)) {
+        const keyChar = key[(i - keyOffset) % key.length];
+        const charCode = (this.alphabet.indexOf(curr) + this.alphabet.indexOf(keyChar)) % 26;
+        return [...acc, this.alphabet[charCode]];
+      } else {
+        keyOffset += 1;
+        return [...acc, curr];
+      }
+    }, []);
+
+    return this.directMachine ? encryptedMessage.join('') : encryptedMessage.reverse().join('');
+  }
+
+  decrypt(message, key) {
+    this.validateArgs(message, key);
+    [message, key] = this.getUpperCasedArgs(message, key);
+
+    let keyOffset = 0;
+
+    const decryptedMessage = [...message].reduce((acc, curr, i) => {
+      if (this.alphabet.includes(curr)) {
+        const keyChar = key[(i - keyOffset) % key.length];
+        const charCode =
+          (this.alphabet.indexOf(curr) - this.alphabet.indexOf(keyChar)) % 26 < 0
+            ? (this.alphabet.indexOf(curr) - this.alphabet.indexOf(keyChar) + 26) % 26
+            : (this.alphabet.indexOf(curr) - this.alphabet.indexOf(keyChar)) % 26;
+        return [...acc, this.alphabet[charCode]];
+      } else {
+        keyOffset += 1;
+        return [...acc, curr];
+      }
+    }, []);
+
+    return this.directMachine ? decryptedMessage.join('') : decryptedMessage.reverse().join('');
   }
 }
 
